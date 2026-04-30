32e8e16823e3b2470c51c9a0baf41752786d22ac
AlanContrera <giovannicontre24@gmail.com>
2026-04-30 13:08:47 -0600

    Ajustes en app

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
-
+const cors = require('cors');
 require('dotenv').config();
+
+// conexión a la base de datos
 require('./config/db');
 
-const app = express();
-app.use(express.json());
-
+// rutas
 const authRoutes = require('./routes/auth.routes');
 
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://backend-auth-2wqrmpkht-giovannicontre24-1013s-projects.vercel.app'
-}));
+const app = express();
 
+// middlewares
+app.use(cors()); 
+app.use(express.json());
+
+// rutas
 app.use('/', authRoutes);
 
+// ruta de prueba
 app.get('/', (req, res) => {
-  res.send('API funcionando');
+  res.send('API funcionando ');
 });
 
+// servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo');
 });
